87c6468ef5818a93b87c182dc0363b049ef7db8a
learnGrowD <dev_willd@naver.com>
2023-11-03 15:28:16 +0900

    sshkey password test

--- api/src/main/java/org/simple_board_willd/api/exceptionhandler/ApiExeptionHandler.java
+++ api/src/main/java/org/simple_board_willd/api/exceptionhandler/ApiExeptionHandler.java
@@ -24,6 +24,7 @@ public class ApiExeptionHandler {
         return ResponseEntity
                 .status(errorCode.getHttpStatusCode())
                 .body(Api.ERROR(errorCode, description));
+        
 
     }
 }
