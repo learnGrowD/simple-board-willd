1ae2f931fbb8e7754ad4314912d879b3fc99bdc6
learnGrowD <dev_willd@naver.com>
2023-11-03 15:09:52 +0900

    objectmapper configuration 등록

---
+++ api/src/main/java/org/simple_board_willd/api/config/objectmapper/ObjectMapperConfig.java
@@ -0,0 +1,11 @@
+package org.simple_board_willd.api.config.objectmapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    
+
+}
